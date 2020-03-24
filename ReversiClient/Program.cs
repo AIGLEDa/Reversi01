5590e97aab54e4eb7aa3bac5612cf7ac5ead154b
AIGLE <damien_aigle@hotmail.com>
2020-03-24 08:54:46 +0100

    version seulement le cadre

--- ReversiClient/Program.cs
+++ ReversiClient/Program.cs
@@ -1,12 +1,89 @@
-﻿using System;
+﻿using ReversiLibrary;
+using System;
+
 
 namespace ReversiClient
 {
     class Program
     {
+
+        private static void DrawLine()
+        {
+            Console.Write("   ");
+            for(int i = 0; i<Game.Size; i++)
+            {
+                Console.Write("+---");
+            }
+            Console.WriteLine("+");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game game = new Game();
+
+            Console.Write("   ");
+
+            for(int col = 0; col<Game.Size; col++)
+            {
+                Console.Write($"  { (Char)('A' + col) } ");
+            }
+            Console.WriteLine("");
+
+            for(int  row = 0; row<Game.Size; row++)
+            {
+                DrawLine();
+                Console.Write($" {row+1} ");
+                for(int col = 0; col<Game.Size; col++)
+                {
+                    var tile = '?';
+                    switch(game.GetTile(col, row))
+                    {
+                        case Game.Empty: tile = ' '; break;
+                        case Game.Black: tile = 'x'; break;
+                        case Game.White: tile = 'o'; break;
+                    }
+                    Console.Write($"| { tile } ");
+                }
+                Console.WriteLine("|");
+            }
+/*
+            for (int j = 0; j < Game.Size; j++)
+            {
+                
+                //Console.Write("*---*");
+                for (int i = 0; i < Game.Size; i++)
+                {
+                    Console.Write("+-+");
+                }
+                Console.Write("\n");
+
+                for (int i = 0; i < Game.Size; i++)
+                {
+                    if(game.GetTile(i, j) == 0)
+                    {
+                        Console.Write("| |");
+                        
+                    }
+
+                    if (game.GetTile(i, j) == -1)
+                    {
+                        Console.Write("|*|");
+
+                    }
+
+                    if (game.GetTile(i, j) == 1)
+                    {
+                        Console.Write("|0|");
+
+                    }
+                    //Console.Write($"|{game.GetTile(i, j)}|");
+                    // Console.Write("+---+");
+                }
+                Console.Write("\n");
+            }
+
+    */
+
+
         }
     }
 }
---
+++ ReversiLibrary/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReversiLibrary
+{
+    public class Game
+    {
+        public const int Size = 8;
+        public const int Empty = 0;
+        public const int Black = 1;
+        public const int White = -1;
+        private int[,] _board = new int[8, 8];
+
+        public Game()
+        {
+            _board[3, 3] = -1;
+            _board[3, 4] = 1;
+            _board[4, 3] = 1;
+            _board[4, 4] = -1;
+            
+        }
+
+        public int GetTile(int col, int row)
+        {
+            return _board[col, row];
+        }
+
+
+    }
+}
